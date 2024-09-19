cc7c43d9a9b29e14f0f45567b373d828236d0aa8
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-19 17:14:19 +0200

    fix product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="product-card">
+    <img :src="`/images/${producto.img}`" alt="Imagen del producto" class="product-image" />
     <h3>{{ producto.nombre }}</h3>
     <p>Precio: {{ producto.precio }}€</p>
-    <p>Material: {{ producto.material }}</p>
+    <p>Descripción: {{ producto.descripcion }}</p>
     <p>Categoria: {{ producto.categoria }}</p>
     <p>Subcategoria: {{ producto.subcategoria }}</p>
     <p>Pasillo: {{ producto.pasillo }}</p>
@@ -24,13 +25,19 @@ export default {
   border: 1px solid #ddd;
   border-radius: 8px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  padding: 20px; 
+  padding: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   aspect-ratio: 1 / 1; /* Mantiene las tarjetas cuadradas */
   overflow: hidden;
-  min-height: 200px; 
+  min-height: 200px;
+}
+.product-image {
+  max-width: 100%;
+  max-height: 150px;
+  object-fit: cover;
+  margin-bottom: 10px;
 }
 h3 {
   font-size: 20px;
@@ -43,3 +50,4 @@ p {
   color: #666;
 }
 </style>
+
