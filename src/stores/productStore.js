7eeb5178783f53d4767343c3240b8b62d3f32d41
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-26 15:11:05 +0200

    add stores product

---
+++ src/stores/productStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { fetchProducts } from '../core/api/product/productService';
+
+export const useProductStore = defineStore('productStore', () => {
+  const products = ref([]);
+
+  const loadProducts = async () => {
+    try {
+      products.value = await fetchProducts(); 
+    } catch (error) {
+      console.error('Error loading products:', error);
+    }
+  };
+
+  return {
+    products,
+    loadProducts, 
+  };
+});
